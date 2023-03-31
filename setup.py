90491b437f09117d911b470e67f6b95bf2485729
Laith Abu-Nawwas <36891818+lanawwas@users.noreply.github.com>
2023-03-31 17:51:45 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,22 @@
-from setuptools import setup
+# Author: Laith Abu-Nawwas
+# Co-author: ChatGPT
+
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+    required = f.read().splitlines()
 
 setup(
     name="twitter-scraper",
     version="0.1",
     description="Scrape Twitter for tweets based on a specified hashtag or keywords",
     author="LANAWWAS / ChatGPT4 Co-Pilot",
-    packages=["twitter_scraper"],
-    install_requires=[
-        "tweepy",
-        "pyyaml",
-        "textblob",
-        "langdetect"
-    ],
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=required,
     entry_points={
-        "console_scripts": [
+        'console_scripts': [
             "twitter-scraper=twitter_scraper.main:main"
         ]
-    }
+    },
 )

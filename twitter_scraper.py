9ce9286ba9d14b8e260b988f520632ceb4e095ab
Laith Abu-Nawwas <36891818+lanawwas@users.noreply.github.com>
2023-03-31 21:24:09 +0200

    Update twitter_scraper.py

--- twitter_scraper.py
+++ twitter_scraper.py
@@ -26,30 +26,9 @@ else:
 # Scrape tweets
 # tweets = tweepy.Cursor(api.search_tweets, q=query, tweet_mode='extended', lang=language).items() // only in extended access API not in basic API v2 endpoint 
 
-# tweets =  gen_request_parameters(query=query, results_per_call=10)
+ tweets =  gen_request_parameters(query=query, results_per_call=10)
 
-rule = gen_request_parameters(query=query, tweet_fields=["created_at", "public_metrics", "author_id", "lang", "conversation_id", 
-                                                         "in_reply_to_user_id", "referenced_tweets"], expansions=["author_id"], user_fields=["username", "public_metrics", "verified"], place_fields=["full_name", "country_code"], media_fields=["duration_ms", "height", "media_key", "public_metrics", "type", "width", "url"])
-tweets = ResultStream(rule_payload=rule,
-                      max_results=10,
-                      tweet_mode='extended').stream()
 
-# Sentiment analysis
-if 'sentiment' in config and config['sentiment']:
-    from langdetect import detect
-    from textblob import TextBlob
-
-    for tweet in tweets:
-        if tweet.full_text.startswith('RT'):
-            continue
-        if detect(tweet.full_text) == 'ar':
-            blob = TextBlob(tweet.full_text)
-            sentiment = blob.sentiment.polarity
-        else:
-            blob = TextBlob(tweet.full_text, analyzer=NaiveBayesAnalyzer())
-            sentiment = blob.sentiment.classification
-        print(f'Tweet: {tweet.full_text}')
-        print(f'Sentiment: {sentiment}\n')
 else:
     # Save tweets to CSV file
     with open('tweets.csv', 'w', encoding='utf-8') as f:
